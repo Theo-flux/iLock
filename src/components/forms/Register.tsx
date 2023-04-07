95b035da74846273b58e90eb8cc11c59a0f26bc5
Theo <ekunnusitheophilus@gmail.com>
2023-04-07 18:05:30 +0100

    fix: production error fix

--- src/components/forms/Register.tsx
+++ src/components/forms/Register.tsx
@@ -1,8 +1,19 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Logo from '../../assets/images/iLock.svg'
-import { AppContainer, Button, InputPasssword, InputText, StyledSmallText, StyledLink } from '../../shared'
+import { AppContainer, Button, InputPasssword, InputText, InputEmail, StyledSmallText, StyledLink } from '../../shared'
 import {TopContainer, FormContainer, MidContainer, LogoImage} from './index.css'
 
 function Register() {
+  const [userInfo, setUserInfo] = useState({ email: '', password: '', username: '' });
+  const [error, setError] = useState({ email: '', password: '', username: '' });
+  const navigate = useNavigate();
+
+  function handleOnchange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.currentTarget;
+
+    setUserInfo({ ...userInfo, [name]: value });
+  }
   return (
     <AppContainer>
       <TopContainer>
@@ -14,9 +25,9 @@ function Register() {
           <LogoImage src={Logo} alt="logo" />
         </MidContainer>
 
-        <InputText id="email" label="Email"/>
-        <InputText id="username" label="Username"/>
-        <InputPasssword id="password" label="Password"/>
+        <InputEmail name="email" id="email" label="Email" onChange={handleOnchange}/>
+        <InputText name="username" id="username" label="username" onChange={handleOnchange}/>
+        <InputPasssword name="password" id="password" label="Password" onChange={handleOnchange}/>
         <Button>Register</Button>
       </FormContainer>
     </AppContainer>

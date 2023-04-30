59660e6f736129964f5fc02151e67a0490d428a8
Theo <ekunnusitheophilus@gmail.com>
2023-04-30 20:01:10 +0100

    feat: notification ui

--- src/components/notifications/Notification.tsx
+++ src/components/notifications/Notification.tsx
@@ -1,10 +1,46 @@
 import { Wrapper } from '../../shared'
 import NavCommon from '../common/NavCommon'
+import { NotificationSection, NotificationPod, Figure, Image, Column, ColumnTitle, ColumnText } from './notification.css'
+import notImg from '../../assets/images/notification_img.svg'
 
 function Notification() {
   return (
     <Wrapper>
       <NavCommon text={'Notifications'}/>
+      <NotificationSection>
+        <NotificationPod>
+          <Figure>
+            <Image src={notImg} alt="admin_intruder"/>
+          </Figure>
+
+          <Column>
+            <ColumnTitle>Admin</ColumnTitle>
+            <ColumnText>Sun, Mar 26 17:09pm</ColumnText>
+          </Column>
+        </NotificationPod>
+
+        <NotificationPod>
+          <Figure>
+            <Image src={notImg} alt="admin_intruder"/>
+          </Figure>
+
+          <Column>
+            <ColumnTitle>Admin</ColumnTitle>
+            <ColumnText>Sun, Mar 26 17:09pm</ColumnText>
+          </Column>
+        </NotificationPod>
+
+        <NotificationPod>
+          <Figure>
+            <Image src={notImg} alt="admin_intruder"/>
+          </Figure>
+
+          <Column>
+            <ColumnTitle>Admin</ColumnTitle>
+            <ColumnText>Sun, Mar 26 17:09pm</ColumnText>
+          </Column>
+        </NotificationPod>
+      </NotificationSection>
     </Wrapper>
   )
 }
--- src/components/notifications/notification.css.ts
+++ src/components/notifications/notification.css.ts
@@ -1,1 +1,26 @@
 import styled from "styled-components";
+import { StyledH5, StyledTinyText } from "../../shared";
+
+export const NotificationSection = styled.section`
+    margin-top: 2rem;
+`
+
+export const NotificationPod = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    
+    &:not(:last-of-type) {
+        margin-bottom: 2rem;
+    }
+`
+
+export const Figure = styled.figure``;
+
+export const Image = styled.img``;
+
+export const Column = styled.div``;
+
+export const ColumnTitle = styled(StyledH5)``;
+
+export const ColumnText = styled(StyledTinyText)``
